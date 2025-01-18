6c344a43e9396196044182e14ca901c82c2fe505
Jeffrey Wood <jeffreywood.dev@gmail.com>
2025-01-18 10:20:16 -0500

    treeincludes dfs

--- treeincludes/index.js
+++ treeincludes/index.js
@@ -20,3 +20,26 @@ const treeIncludes = (root, target) => {
 
 	return false;
 };
+
+const treeIncludesDFS = (root, target) => {
+	if (root === null) {
+		return false;
+	}
+	let queue = [root];
+
+	while (queue.length > 0) {
+		let cur = queue.shift();
+		if (cur.val === target) {
+			return true;
+		} else {
+			if (cur.left !== null) {
+				queue.push(cur.left);
+			}
+			if (cur.right !== null) {
+				queue.push(cur.right);
+			}
+		}
+	}
+
+	return false;
+};
