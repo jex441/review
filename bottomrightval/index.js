cbdc7043ff1fe21a4dc60c0200b15c3ab298b4fd
Jeffrey Wood <jeffreywood.dev@gmail.com>
2025-01-20 14:54:04 -0500

    brv simple

--- bottomrightval/index.js
+++ bottomrightval/index.js
@@ -1,22 +1,33 @@
-const bottomRightValue = (root) => {
-	let map = { level: -1, val: 0 };
-	let count = -1;
-	let queue = [root];
+// const bottomRightValue = (root) => {
+// 	let map = { level: -1, val: 0 };
+// 	let count = -1;
+// 	let queue = [root];
 
+// 	while (queue.length > 0) {
+// 		let length = queue.length;
+// 		count += 1;
+// 		for (let i = 0; i < length; i++) {
+// 			let cur = queue.shift();
+// 			if ((i = queue.length - 1)) {
+// 				if (count > map.level) {
+// 					map.level = count;
+// 					map.val = cur.val;
+// 				}
+// 			}
+// 			if (cur.left !== null) queue.push(cur.left);
+// 			if (cur.right !== null) queue.push(cur.right);
+// 		}
+// 	}
+// 	return map.val;
+// };
+
+const bottomRightValue = (root) => {
+	let queue = [root];
+	let cur = null;
 	while (queue.length > 0) {
-		let length = queue.length;
-		count += 1;
-		for (let i = 0; i < length; i++) {
-			let cur = queue.shift();
-			if ((i = queue.length - 1)) {
-				if (count > map.level) {
-					map.level = count;
-					map.val = cur.val;
-				}
-			}
-			if (cur.left !== null) queue.push(cur.left);
-			if (cur.right !== null) queue.push(cur.right);
-		}
+		cur = queue.shift();
+		if (cur.left !== null) queue.push(cur.left);
+		if (cur.right !== null) queue.push(cur.right);
 	}
-	return map.val;
+	return cur.val;
 };
