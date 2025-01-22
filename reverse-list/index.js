3662e4580126524502f93f9ee186d7f98bc11e8b
Jeffrey Wood <jeffreywood.dev@gmail.com>
2025-01-22 13:55:32 -0500

    repeat

--- reverse-list/index.js
+++ reverse-list/index.js
@@ -20,3 +20,25 @@ const reverseList = (head) => {
 //     prev = head;
 //     return reverseList(next, prev)
 //   };
+
+// const reverseLinkedList = (head, prev = null) => {
+//     if(head === null) return prev;
+//     let next = head.next;
+//     head.next = prev;
+//     prev = head;
+//     return reverseLinkedList(next, prev)
+//   }
+
+//   const reverseLinkedListIterative = (head) => {
+//     let current = head;
+//     let prev = null;
+
+//     while(current !== null) {
+//       let next = head.next;
+//       current.next = prev;
+//       prev = current;
+//       current = next;
+//     }
+
+//     return prev;
+//   }
